e20fd341112c2df74fc93df565005110ec70ff92
Peter <petercblanco@gmail.com>
2015-02-27 19:55:07 -0500

    init commit

---
+++ BPlusTree.java
@@ -0,0 +1,119 @@
+import java.util.AbstractMap;
+import java.util.Map.Entry;
+
+/**
+ * BPlusTree Class Assumptions: 1. No duplicate keys inserted 2. Order D:
+ * D<=number of keys in a node <=2*D 3. All keys are non-negative
+ */
+public class BPlusTree<K extends Comparable<K>, T> {
+
+	public Node<K,T> root;
+	public static final int D = 2;
+
+	private T searchHelper(K key, Node<K,T> node){
+		if (node.isLeafNode){
+			LeafNode<K, T> lnode = (LeafNode<K,T>)node;
+			//iterate through keys to find a match
+			for (int i=0; i < lnode.keys.size(); i++){
+				K fetchedKey = lnode.keys.get(i);
+				if (key.compareTo(fetchedKey) == 0){
+					return lnode.values.get(i);
+				} else if (key.compareTo(fetchedKey) > 0 ){ // Break if the key is smaller then fetched (this means no matches)
+					return null;
+				}
+			}
+		} else {
+			
+		}
+		return null;
+	}
+	
+	/**
+	 * TODO Search the value for a specific key
+	 * 
+	 * @param key
+	 * @return value
+	 */
+	public T search(K key) {
+		return this.searchHelper(key, root);
+	}
+
+	/**
+	 * TODO Insert a key/value pair into the BPlusTree
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public void insert(K key, T value) {
+
+	}
+
+	/**
+	 * TODO Split a leaf node and return the new right node and the splitting
+	 * key as an Entry<slitingKey, RightNode>
+	 * 
+	 * @param leaf
+	 * @return the key/node pair as an Entry
+	 */
+	public Entry<K, Node<K,T>> splitLeafNode(LeafNode<K,T> leaf) {
+
+		return null;
+	}
+
+	/**
+	 * TODO split an indexNode and return the new right node and the splitting
+	 * key as an Entry<slitingKey, RightNode>
+	 * 
+	 * @param index
+	 * @return new key/node pair as an Entry
+	 */
+	public Entry<K, Node<K,T>> splitIndexNode(IndexNode<K,T> index) {
+
+		return null;
+	}
+
+	/**
+	 * TODO Delete a key/value pair from this B+Tree
+	 * 
+	 * @param key
+	 */
+	public void delete(K key) {
+
+	}
+
+	/**
+	 * TODO Handle LeafNode Underflow (merge or redistribution)
+	 * 
+	 * @param left
+	 *            : the smaller node
+	 * @param right
+	 *            : the bigger node
+	 * @param parent
+	 *            : their parent index node
+	 * @return the splitkey position in parent if merged so that parent can
+	 *         delete the splitkey later on. -1 otherwise
+	 */
+	public int handleLeafNodeUnderflow(LeafNode<K,T> left, LeafNode<K,T> right,
+			IndexNode<K,T> parent) {
+		return -1;
+
+	}
+
+	/**
+	 * TODO Handle IndexNode Underflow (merge or redistribution)
+	 * 
+	 * @param left
+	 *            : the smaller node
+	 * @param right
+	 *            : the bigger node
+	 * @param parent
+	 *            : their parent index node
+	 * @return the splitkey position in parent if merged so that parent can
+	 *         delete the splitkey later on. -1 otherwise
+	 */
+	public int handleIndexNodeUnderflow(IndexNode<K,T> leftIndex,
+			IndexNode<K,T> rightIndex, IndexNode<K,T> parent) {
+		return -1;
+	}
+
+}
